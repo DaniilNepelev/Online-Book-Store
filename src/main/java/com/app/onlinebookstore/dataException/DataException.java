8d3e42d26b7c6d16a377d798492b19e004f89d61
DaniilNepelev <kakashilvng@gmail.com>
2023-09-04 00:34:52 +0300

    cancel

--- src/main/java/com/app/onlinebookstore/dataException/DataException.java
+++
@@ -1,7 +0,0 @@
-package com.app.onlinebookstore.dataException;
-
-public class DataException extends RuntimeException {
-    public DataException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
